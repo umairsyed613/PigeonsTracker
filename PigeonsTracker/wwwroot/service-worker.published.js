aad1ae3e5e281b0c744b8c47c75de417317d9d78
Umair Syed <naqvi.umair@gmail.com>
2021-06-15 15:24:58 +0200

    service worker fix 08

--- PigeonsTracker/wwwroot/service-worker.published.js
+++ PigeonsTracker/wwwroot/service-worker.published.js
@@ -10,7 +10,7 @@ self.addEventListener('beforeinstallprompt', event => event.respondWith(beforeIn
 const cacheNamePrefix = 'pt-offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
-const offlineAssetsExclude = [ /^service-worker\.js$/ ];
+const offlineAssetsExclude = [ /^service-worker\.js$/,/^staticwebapp.config\.js$/ ];
 
 async function onInstall(event) {
     console.info('Service worker: Install');
