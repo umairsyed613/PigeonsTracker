f686336e36be49d78cf92dbe1872ea68919c6500
syed.naqvi <syed.naqvi@visma.com>
2024-04-21 01:59:23 +0200

    design changes

--- PigeonsTracker/Services/AppState.cs
+++ PigeonsTracker/Services/AppState.cs
@@ -6,7 +6,7 @@ namespace PigeonsTracker.Services
 {
     public class AppState
     {
-        public double ApplicationVersion { get; private set; } = 2.0;
+        public double ApplicationVersion { get; private set; } = 2.1;
 
         public OpenWeatherApiResult CachedOpenWeatherApiResult { get; set; }
         public Location Location { get; private set; }
