ca626e533b8607cb78ad54ca85d3904e732cf750
UmairNaqvi <naqvi.umair@gmail.com>
2024-05-12 15:13:19 +0200

    bug fix

--- PigeonsTracker/Services/AppState.cs
+++ PigeonsTracker/Services/AppState.cs
@@ -7,7 +7,7 @@ namespace PigeonsTracker.Services;
 
 public class AppState
 {
-    public double ApplicationVersion { get; private set; } = 3.21;
+    public double ApplicationVersion { get; private set; } = 3.22;
     public bool IsMobile { get; set; }
     public OpenWeatherApiResult CachedOpenWeatherApiResult { get; set; }
     public Location Location { get; private set; }
